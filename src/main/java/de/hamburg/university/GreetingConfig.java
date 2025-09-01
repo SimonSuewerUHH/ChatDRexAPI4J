a1e79865284706db7713adc8e5b9744b1585a1ff
Simon Süwer <simon.suewer@uni-hamburg.de>
2025-09-01 11:44:44 +0200

    init via code.quarkus.io

---
+++ src/main/java/de/hamburg/university/GreetingConfig.java
@@ -0,0 +1,12 @@
+package de.hamburg.university;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
