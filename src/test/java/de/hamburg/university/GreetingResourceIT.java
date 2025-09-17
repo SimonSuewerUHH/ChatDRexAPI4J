1f76aa8e687c4ad6db2d9e6cbea4a97dcf692dc7
Simon Süwer <simon.suewer@uni-hamburg.de>
2025-09-17 20:45:09 +0200

    Update README.md to enhance project description, configuration details, and API documentation

--- src/test/java/de/hamburg/university/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package de.hamburg.university;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
