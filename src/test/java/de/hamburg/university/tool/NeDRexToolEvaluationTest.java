0718b52381097a81771db76312cc38588c2ce69f
Simon Süwer <simon.suewer@uni-hamburg.de>
2025-09-24 11:54:33 +0200

    Update JSON files for Closeness and TrustRank analyses; modify test cases in NeDRexToolEvaluationTest to include TrustRank and Closeness tests

--- src/test/java/de/hamburg/university/tool/NeDRexToolEvaluationTest.java
+++ src/test/java/de/hamburg/university/tool/NeDRexToolEvaluationTest.java
@@ -2,7 +2,6 @@ package de.hamburg.university.tool;
 
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.hamburg.university.agent.bot.NeDRexBot;
 import de.hamburg.university.agent.bot.NeDRexToolDecisionBot;
 import de.hamburg.university.agent.tool.nedrex.NeDRexTool;
@@ -11,12 +10,15 @@ import de.hamburg.university.helper.AIJudgeBot;
 import de.hamburg.university.helper.JsonLoader;
 import de.hamburg.university.helper.drugstone.DrugstOneGraphHelper;
 import de.hamburg.university.helper.drugstone.DrugstOneNetworkDTO;
+import de.hamburg.university.service.nedrex.closeness.ClosenessResultDTO;
 import de.hamburg.university.service.nedrex.diamond.DiamondResultsDTO;
+import de.hamburg.university.service.nedrex.trustrank.TrustRankResultDTO;
 import de.hamburg.university.tool.pojo.NeDRexToolQuestion;
 import de.hamburg.university.tool.pojo.NeDRexToolTestResult;
 import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -40,17 +42,53 @@ public class NeDRexToolEvaluationTest {
     @Inject
     AIJudgeBot judgeBot;
 
-    private static final ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+    List<NeDRexToolTestResult> results = new ArrayList<>();
 
 
     @Test
+    @Order(1)
     void agentDiamondTest() {
         List<NeDRexToolQuestion> questions = JsonLoader.loadJson("tools/nedrex/diamond/questions.json", new TypeReference<List<NeDRexToolQuestion>>() {
         });
 
-        List<NeDRexToolTestResult> results = new ArrayList<>();
         for (NeDRexToolQuestion question : questions) {
             NeDRexToolTestResult result = testDiamond(question);
+            results.add(result);
+            Log.info(result.toString());
+        }
+    }
+
+    @Test
+    @Order(2)
+    void agentTrustrankTest() {
+        List<NeDRexToolQuestion> questions = JsonLoader.loadJson("tools/nedrex/trustrank/questions.json", new TypeReference<List<NeDRexToolQuestion>>() {
+        });
+
+        for (NeDRexToolQuestion question : questions) {
+            NeDRexToolTestResult result = testTrustrank(question);
+            results.add(result);
+            Log.info(result.toString());
+        }
+    }
+
+    @Test
+    @Order(3)
+    void agentClosenessTest() {
+        List<NeDRexToolQuestion> questions = JsonLoader.loadJson("tools/nedrex/closeness/questions.json", new TypeReference<List<NeDRexToolQuestion>>() {
+        });
+
+        for (NeDRexToolQuestion question : questions) {
+            NeDRexToolTestResult result = testCloseness(question);
+            results.add(result);
+            Log.info(result.toString());
+        }
+    }
+
+    @Test
+    @Order(4)
+    void getResult() {
+        Log.info("Final Results:");
+        for (NeDRexToolTestResult result : results) {
             results.add(result);
             Log.info(result.toString());
         }
@@ -65,7 +103,7 @@ public class NeDRexToolEvaluationTest {
 
         String enhancedContext = neDRexBot.answer(question.getPath(), question.getQuestion(), "");
         NeDRexToolDecisionResult decision = neDRexToolDecisionBot.answer(question.getQuestion(), enhancedContext);
-        if (decision.getEntrezIds().isEmpty()) {
+        if (decision.getEntrezIds() == null || decision.getEntrezIds().isEmpty()) {
             return result;
         }
         List<String> entrezIds = decision.getEntrezIds();
@@ -87,8 +125,74 @@ public class NeDRexToolEvaluationTest {
         return result;
     }
 
+    private NeDRexToolTestResult testTrustrank(NeDRexToolQuestion question) {
+        NeDRexToolTestResult result = new NeDRexToolTestResult(question.getQuestion(), question.getPath());
+        String path = "tools/nedrex/trustrank/" + question.getPath();
+        TrustRankResultDTO resultMocked = JsonLoader.loadJson(path, new TypeReference<TrustRankResultDTO>() {
+        });
+
+
+        String enhancedContext = neDRexBot.answer(question.getPath(), question.getQuestion(), "");
+        NeDRexToolDecisionResult decision = neDRexToolDecisionBot.answer(question.getQuestion(), enhancedContext);
+        if (decision.getEntrezIds() == null || decision.getEntrezIds().isEmpty()) {
+            return result;
+        }
+        List<String> entrezIds = decision.getEntrezIds();
+        checkInput(resultMocked.getSeedProteins(), entrezIds, result);
+        if (!result.isCorrectInput()) {
+            return result;
+        }
+        if (!decision.getToolName().equalsIgnoreCase("trustrank")) {
+            return result;
+        }
+        result.setCorrectTool(true);
+
+        //As we now that the input correct, we can assume that the output is correct if the tool is called
+        DrugstOneNetworkDTO network = drugstOneGraphHelper.trustrankToNetwork(resultMocked);
+        if (network.getNodes().isEmpty()) {
+            return result;
+        }
+        result.setCorrectAnswer(true);
+        return result;
+    }
+
+    private NeDRexToolTestResult testCloseness(NeDRexToolQuestion question) {
+        NeDRexToolTestResult result = new NeDRexToolTestResult(question.getQuestion(), question.getPath());
+        String path = "tools/nedrex/closeness/" + question.getPath();
+        ClosenessResultDTO resultMocked = JsonLoader.loadJson(path, new TypeReference<ClosenessResultDTO>() {
+        });
+
+
+        String enhancedContext = neDRexBot.answer(question.getPath(), question.getQuestion(), "");
+        NeDRexToolDecisionResult decision = neDRexToolDecisionBot.answer(question.getQuestion(), enhancedContext);
+        if (decision.getEntrezIds() == null || decision.getEntrezIds().isEmpty()) {
+            return result;
+        }
+        List<String> entrezIds = decision.getEntrezIds();
+        checkInput(resultMocked.getSeedProteins(), entrezIds, result);
+        if (!result.isCorrectInput()) {
+            return result;
+        }
+        if (!decision.getToolName().equalsIgnoreCase("closeness")) {
+            return result;
+        }
+        result.setCorrectTool(true);
+
+        //As we now that the input correct, we can assume that the output is correct if the tool is called
+        DrugstOneNetworkDTO network = drugstOneGraphHelper.trustrankToNetwork(resultMocked);
+        if (network.getNodes().isEmpty()) {
+            return result;
+        }
+        result.setCorrectAnswer(true);
+        return result;
+    }
+
     private void checkInput(List<String> list1, List<String> list2, NeDRexToolTestResult result) {
         boolean correctInput = true;
+        if (list1 == null || list2 == null) {
+            result.setCorrectInput(false);
+            return;
+        }
         if (list1.size() != list2.size()) {
             correctInput = false;
         }
