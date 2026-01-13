973c49a1da9e39be3c92923752a2fbb6046095e5
Simon Süwer <simon.suewer@uni-hamburg.de>
2026-01-13 15:06:00 +0100

    Add JsonIgnoreProperties annotation to ChatRequestDTO for unknown propertiesAnnotate `ChatRequestDTO` with `@JsonIgnoreProperties` to ignore unknown properties

--- src/main/java/de/hamburg/university/api/chat/messages/ChatRequestDTO.java
+++ src/main/java/de/hamburg/university/api/chat/messages/ChatRequestDTO.java
@@ -1,6 +1,7 @@
 package de.hamburg.university.api.chat.messages;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ChatRequestDTO {
 
     private String token;
